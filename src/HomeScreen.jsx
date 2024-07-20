6a48fcbc383f4de57158c295977959cdc382b0a9
tayeb7 <omer.tayeb.a@gmail.com>
2024-07-20 03:55:45 +0100

    organizing files

--- src/HomeScreen.jsx
+++ src/HomeScreen.jsx
@@ -1,8 +1,8 @@
-import Quote from "../components/Quote.jsx";
-import Skills from "../components/Skills.jsx";
-import Footer from "../components/Footer.jsx";
-import Landing from "../components/Landing.jsx";
-import Contact from "../components/Contact.jsx";
+import Quote from "../components/components-home-screen/Quote.jsx";
+import Skills from "../components/components-home-screen/Skills.jsx";
+import Footer from "../components/components-home-screen/Footer.jsx";
+import Landing from "../components/components-home-screen/Landing.jsx";
+import Contact from "../components/components-home-screen/Contact.jsx";
 
 
 export default function HomeScreen() {
